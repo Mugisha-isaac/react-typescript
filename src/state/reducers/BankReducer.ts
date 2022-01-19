d3cefc9387ce84d99a6636339dab6cfd778daecb
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-01-19 19:29:41 +0200

    added combineReducers

--- src/state/reducers/BankReducer.ts
+++ src/state/reducers/BankReducer.ts
@@ -2,7 +2,7 @@ import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
 const initialState = 0;
-const reducer = (state:number = initialState,action:Action) =>{
+const bankReducer = (state:number = initialState,action:Action) =>{
    switch(action.type){
        case ActionType.DEPOSIT:
            return state + action.payload;
@@ -19,4 +19,4 @@ const reducer = (state:number = initialState,action:Action) =>{
 }
 
 
-export default reducer;
+export default bankReducer;
